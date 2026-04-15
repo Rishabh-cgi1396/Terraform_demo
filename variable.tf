7802dd88ce847c439dfa55275ffdbc94f59ad57a
Rishabh-cgi1396 <rishabh.anand@cgi.com>
2026-04-15 16:35:50 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,21 +1,20 @@
 variable "vm_name" {}
 variable "location" {}
 variable "resource_group_name" {}
+variable "subnet_id" {}
 
-variable "vm_size" {
-  default = "Standard_B1s"
-}
-
-variable "admin_username" {
-  default = "azureuser"
-}
-
+variable "vm_size" {}
+variable "admin_username" {}
 variable "ssh_public_key" {}
 
-variable "create_public_ip" {
-  default = true
-}
+variable "create_public_ip" {}
+variable "create_nsg" {}
 
-variable "create_nsg" {
-  default = true
+variable "image" {
+  type = object({
+    publisher = string
+    offer     = string
+    sku       = string
+    version   = string
+  })
 }
